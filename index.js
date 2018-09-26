b197f6a7a3f0bbf38e9fdf40b319ccc952523f15
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-09-26 16:21:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,17 +20,11 @@ function sayHiToGrandma(string) {
 function sayHiToGrandma(string) {
   console.log('HELLO')
   return string.toUpperCase("YES INDEED!")
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', 
-function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
+}
+function sayHiToGrandma(string) {
+  console.log("I love you, Grandma.")
+  return string("I love you, too.")
+}
 })
 
 
