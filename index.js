7823be2db6822a0a703ae96f8347125930b173d4
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-09-26 16:53:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
 }
 describe('sayHiToGrandma(string)', 
 function sayHiToGrandma(string) {
-  console.log(`'hello', ${sayHiToGrandma}`)
+  console.log('hello')
   expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
 })
 describe('sayHiToGrandma(string)',
